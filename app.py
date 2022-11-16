f59c6611d49d7fc8cad3a372cdaeb8a15a96139a
JeannieLan <117909693+JeannieLan@users.noreply.github.com>
2022-11-16 22:04:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def inputAddRecord(msg,user_id): #這邊變數是輸入的訊息=記帳,會retur
             type_=dailyCost.split(' ')[1]
             money=dailyCost.split(' ')[2]
             
-            connect = psycopg2.connect("count.db")
+            connect = sqlite3.connect("count.db")
             cursor = connect.cursor()
             sql="INSERT INTO count(id,time,type,money) values('%s','%s','%s','%s')" % (user_id,timeStr,type_,money)
             cursor.execute(sql)
